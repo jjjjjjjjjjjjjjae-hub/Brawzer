be3212e4c1269eaa5615bc82dcc1178acd7973bc
jjjjjjjjjjjjjjae-hub <jjjjjjjjjjjjjjae@gmail.com>
2026-04-17 15:59:23 +0500

    Final permission fix for OTA

--- android/app/src/main/java/com/bro/ghost/MainActivity.java
+++ android/app/src/main/java/com/bro/ghost/MainActivity.java
@@ -1,5 +1,20 @@
 package com.bro.ghost;
 
+import android.os.Bundle;
+import android.util.Log;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Қолданба ашылғанда жаңартуды тексеру
+        checkUpdates();
+    }
+
+    private void checkUpdates() {
+        Log.d("OTA", "Жаңарту тексерілуде...");
+        // Бұл жерге кейінірек жүктеу логикасын толықтырамыз
+    }
+}
